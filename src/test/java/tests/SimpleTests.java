dc09c03ee9e01487ab51db8a2d1c580ebc5f21f0
berlioz458 <berlioz458@gmail.com>
2022-01-24 21:26:32 +0700

    Initial commit

---
+++ src/test/java/tests/SimpleTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTests {
+    @Test
+    void test01() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test02() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test03() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test04() {
+        assertTrue(true);
+    }
+}
